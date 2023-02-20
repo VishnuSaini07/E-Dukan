dbdbcc58608468514b6df7ea478c0f748434ed8c
Vishnu <vis.saini2015@gmail.com>
2023-02-20 18:39:50 +0530

    shop page created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import ProductDetail from './pages/ProductDetail';
+import Shop from './pages/Shop';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
             <Route path='productDetail' element={<ProductDetail />} />
+            <Route path='shop' element={<Shop />} />
           </Route>
         </Routes>
       </Router>
