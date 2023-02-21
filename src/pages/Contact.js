00197a1ea64607e893488090aec17fbc40049592
vani-vj <vanijain227@gmail.com>
2023-02-21 15:32:00 +0530

    About us

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,28 +1,32 @@
-          import React from 'react'
+import React from 'react'
 
 const Contact = () => {
   return (
-    <div className ="contianer" style={{width: '40%'}} >
-     <h1 className = "my-3"> <center>Contact US</center></h1>
-    <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Full Name</label>
-    <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Name"/>
-</div>
-<div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Mobile Number</label>
-    <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="Mobile"/>
-</div>
-<div className="mb-3">
+    <>
+      <div className="contianer mx-auto" style={{ width: '40%' }} >
+        <h1 className="my-3"> <center>Contact US</center></h1>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Full Name</label>
+          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Mobile Number</label>
+          <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="Mobile" />
+        </div>
+        <div className="mb-3">
 
-  <label for="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-</div>
-<div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Your concern</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-</div>
+          <label for="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">Your concern</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+      </div>
+    </>
+
   )
 }
 
-          export default Contact
+export default Contact
