04394c0436227c4f24f6127ed73e35cff660ac88
vani-vj <vanijain227@gmail.com>
2023-02-21 15:13:24 +0530

    About us

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className ="contianer" style={{width: '40%'}} >
+     <h1 className = "my-3"> <center>Contact US</center></h1>
+    <div className="mb-3">
+    <label for="exampleFormControlInput1" className="form-label">Full Name</label>
+    <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Name"/>
+</div>
+<div className="mb-3">
+    <label for="exampleFormControlInput1" className="form-label">Mobile Number</label>
+    <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="Mobile"/>
+</div>
+<div className="mb-3">
+
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Your concern</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
   )
 }
 
